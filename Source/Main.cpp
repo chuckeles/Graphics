00615eab08e7900b3a76f9fa089f32e753cc7763
Martin <martin@jakubik.info>
2013-09-03 17:13:40 +0200

    Simple projection matrix

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -4,11 +4,19 @@
 #include <SFML\Window\Event.hpp>
 
 #include <gl\GL.h>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	// create window
 	sf::Window window(sf::VideoMode(800, 600), "OpenGL Laboratory");
+
+	// set projection matrix
+	glm::mat4 proj = glm::ortho(0.f, 800.f, 0.f, 600.f);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf(&proj[0][0]);
 
 	// begin loop
 	bool looping = true;
